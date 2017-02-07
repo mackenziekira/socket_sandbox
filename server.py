7b9827758700054a53fa40d6c5c09fd74caee73b
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-02-06 19:33:54 -0800

    crash course in web sockets

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sockets import Sockets
+
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        ws.send(message)
+
+clients =[]
+
+@sockets.route('/chatroom')
+def chatroom(ws):
+    # anyone who visits this route will become a client
+    clients.append(ws)
+
+    while not ws.closed:
+        message = ws.receive()
+
+        for client in clients:
+            client.send(message)
+        
+    clients.remove(ws)
