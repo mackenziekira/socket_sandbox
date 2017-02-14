51d2a730c02d69f817e27101a801120cfefc0713
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-02-13 23:06:59 -0800

    can run app locally, but messages aren't displaying and there are no js errors in console. need to debug

--- websockets/chat.py
+++ websockets/chat.py
@@ -5,10 +5,10 @@ import logging
 import redis
 import gevent
 from flask import Flask, render_template
-from flask_sockets import flask_sockets
+from flask_sockets import Sockets
 from chat_class import ChatBackend
 
-REDIS_URL = os.environ['REDIS_URL']
+REDIS_URL = 'redis://localhost:6379'
 REDIS_CHAN = 'chat'
 
 app = Flask(__name__)
@@ -16,6 +16,42 @@ app.debug = 'DEBUG' in os.environ
 
 sockets = Sockets(app)
 redis = redis.from_url(REDIS_URL)
+
+class ChatBackend(object):
+    """registers and updates websocket clients"""
+
+    def __init__(self):
+        self.clients = list()
+        self.pubsub = redis.pubsub()
+        self.pubsub.subscribe(REDIS_CHAN)
+
+    def __iter_data(self):
+        for message in self.pubsub.listen():
+            data = message.get('data')
+            if message['type'] == 'message':
+                app.logger.info(u'sending message: {}'.format(data))
+                yield data
+
+    def register(self, client):
+        """register a client websocket connection for redis updates"""
+        self.clients.append(client)
+
+    def send(self, client, data):
+        """send data to client, or removes client from clients list if no longer a valid connection"""
+        try:
+            client.send(data)
+        except Exception:
+            self.clients.remove(client)
+
+    def run(self):
+        """listen for new messages in redis, sends them to clients"""
+        for data in self.__iter_data():
+            for client in self.clients:
+                gevent.spawn(self.send, client, data)
+
+    def start(self):
+        """maintain redis subscription in background"""
+        gevent.spawn(self.run)
 
 chats = ChatBackend()
 chats.start()
@@ -29,7 +65,7 @@ def inbox(ws):
     """recieve incoming chat messages, insert them in redis"""
     while not ws.closed:
         gevent.sleep(0.1)
-        message = ws.recieve()
+        message = ws.receive()
 
     if message:
         app.logger.info(u'Inserting message: {}'.format(message))
@@ -42,3 +78,4 @@ def outbox(ws):
 
     while not ws.closed:
         gevent.sleep(0.1)
+
